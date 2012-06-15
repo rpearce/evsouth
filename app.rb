ee67c5632524b1b732adee16d83aa43a606276cf
Robert Pearce <robertwaltonpearce@gmail.com>
2012-06-15 17:19:09 -0400

    added page routing

--- app.rb
+++ app.rb
@@ -5,3 +5,19 @@ require 'haml'
 get '/' do
   haml :index
 end
+
+get '/products' do
+  haml :products
+end
+
+get '/services' do
+  haml :services
+end
+
+get '/about' do
+  haml :about
+end
+
+get '/contact' do
+  haml :contact
+end
